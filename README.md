5316dfbd5bed5e391397af91dab2eebf5c5aa02a
varunu28 <varun.u28@gmail.com>
2018-10-08 14:47:35 -0700

    Added working script

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Twitter-HashTag-Streamer
 A Python script to stream tweets related to a Hashtag ans save it to an S3 bucket
 
+![Demo](demo.gif)
+
 ## To-do
  - [X] Get Twitter API key
  - [X] Create streaming application to stream live tweets
- - [ ] Create S3 config using boto framework
- - [ ] Save every new tweet as a JSON to the S3 bucket
+ - [X] Create S3 config using boto framework
+ - [X] Save every new tweet as a JSON to the S3 bucket
 
 ## How to run the  script?
 
+ - Install the required libraries by running ```pip install -r requirements.txt``` in a virtual environment.
  - Create a Twitter application on the Twitter developer portal
  - Generate the token and and key for the application
  - Create a file ```twitter_credentials.py``` and fill the key and tokens in below template
@@ -18,4 +21,6 @@ A Python script to stream tweets related to a Hashtag ans save it to an S3 bucke
 	CONSUMER_KEY = ""
 	CONSUMER_SECRET = ""
  	```
+ - Make sure to have s3 permissions for the aws user which has been configured on your system
+ - Update the hashtags and bucket name in ```config.yaml```
  - Run the script
--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -2,7 +2,15 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-import twitter_credentials
+import credentials
+import random
+import string
+import os
+import boto3
+import yaml
+
+with open('config.yaml', "r") as config:
+    script_config = yaml.load(config)
 
 
 class TwitterStreamer():
@@ -16,8 +24,8 @@ class TwitterStreamer():
 	def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
 		# Authentication logic
 		listener = StdOutListener(fetched_tweets_filename)
-		auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-		auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+		auth = OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+		auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
 
 		stream = Stream(auth, listener)
 
@@ -35,8 +43,16 @@ class StdOutListener(StreamListener):
 	def on_data(self, data):
 		try:
 			print(data)
-			with open(self.fetched_tweets_filename, 'a') as file:
+
+			# Generating a random prefix
+			random_str = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+			filename = random_str + self.fetched_tweets_filename
+
+			with open(str(filename), 'w') as file:
 				file.write(data)
+			
+			self.upload_to_s3(filename)
+
 		except BaseException as e:
 			print("Error: {}".format(str(e)))
 
@@ -45,11 +61,20 @@ class StdOutListener(StreamListener):
 	def on_error(self, status):
 		print(status)
 
+	def upload_to_s3(self, filename):
+		bucket_name = script_config['bucket_name']
+		output_name = str(filename)
+		s3 = boto3.client('s3')
+
+		s3.upload_file(str(filename), str(bucket_name), output_name)
+		
+		os.remove(str(filename))
+
 
 if __name__ == "__main__":
 
-	hash_tag_list = ['#cmpe295A']
-	fetched_tweets_filename = "tweets.json"
+	hash_tag_list = script_config['hash_tag_list']
+	fetched_tweets_filename = script_config['fetched_tweets_filename']
 
 	twitter_streamer = TwitterStreamer()
 	twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
